7bebe5bc7414dbf177d68915459e33872d7ba2d6
beikeshell <beikeshell@126.com>
2020-07-23 23:12:21 +0800

    add comment

--- UnityEngine.UI/UI/Core/Layout/ILayoutElement.cs
+++ UnityEngine.UI/UI/Core/Layout/ILayoutElement.cs
@@ -8,31 +8,31 @@ namespace UnityEngine.UI
     /// </summary>
     /// <remarks>
     /// The layout system will invoke CalculateLayoutInputHorizontal before querying minWidth, preferredWidth, and flexibleWidth.
-    /// It can potentially save performance if these properties are cached when CalculateLayoutInputHorizontal is invoked,
-    /// so they don't need to be recalculated every time the properties are queried.
+    ///     It can potentially save performance if these properties are cached when CalculateLayoutInputHorizontal is invoked,
+    ///     so they don't need to be recalculated every time the properties are queried.
     ///
     /// The layout system will invoke CalculateLayoutInputVertical before querying minHeight, preferredHeight, and flexibleHeight.
-    /// It can potentially save performance if these properties are cached when CalculateLayoutInputVertical is invoked,
-    /// so they don't need to be recalculated every time the properties are queried.
+    ///     It can potentially save performance if these properties are cached when CalculateLayoutInputVertical is invoked,
+    ///     so they don't need to be recalculated every time the properties are queried.
     ///
     /// The minWidth, preferredWidth, and flexibleWidth properties 【should not】 rely on any properties
-    ///     of the RectTransform which the layout element belong,
-    /// otherwise the behavior will be non-deterministic. 【deterministic 确定的】
+    ///     of the RectTransform which the layout element belong, otherwise the behavior will be non-deterministic. 【deterministic 确定的】
     ///
-    /// The minHeight, preferredHeight, and flexibleHeight properties may rely on horizontal aspects of the RectTransform,
-    /// such as the width or the X component of the position.
-    /// Any properties of the RectTransforms on child layout elements may always be relied on.
+    /// The minHeight, preferredHeight, and flexibleHeight properties 【may】 rely on horizontal aspects of the RectTransform,
+    ///     such as the width or the X component of the position.
+    ///
+    /// Any properties of the RectTransforms on child layout elements may 【always】 be relied on.
     /// </remarks>
     public interface ILayoutElement
     {
         /// <summary>
-        /// After this method is invoked, layout horizontal input properties should return up-to-date values.
-        ///  Children will already have up-to-date layout horizontal inputs when this methods is called.
+        /// After this method is invoked, layout 【horizontal input properties】 should return up-to-date values.
+        /// Children will already have up-to-date layout horizontal inputs when this methods is called.
         /// </summary>
         void CalculateLayoutInputHorizontal();
 
         /// <summary>
-        ///After this method is invoked, layout vertical input properties should return up-to-date values.
+        ///After this method is invoked, layout 【vertical input properties】 should return up-to-date values.
         ///Children will already have up-to-date layout vertical inputs when this methods is called.
         /// </summary>
         void CalculateLayoutInputVertical();
@@ -134,7 +134,7 @@ namespace UnityEngine.UI
         /// If multiple components on the same GameObject implement the ILayoutElement interface,
         /// the values provided by components that return a higher priority value are given priority.
         /// However, values less than zero are ignored.
-        /// This way a component can override only select properties by leaving the remaining values to be -1 or other values less than zero.
+        ///     This way a component can override only select properties by leaving the remaining values to be -1 or other values less than zero.
         /// </remarks>
         int layoutPriority { get; }
     }
@@ -146,15 +146,16 @@ namespace UnityEngine.UI
     /// If a component is driving its own RectTransform it should implement the interface [[ILayoutSelfController]].
     /// If a component is driving the RectTransforms of its children, it should implement [[ILayoutGroup]].
     ///
-    /// The layout system will first invoke SetLayoutHorizontal and then SetLayoutVertical.
+    /// ** The layout system will first invoke SetLayoutHorizontal and then SetLayoutVertical. **
     ///
     /// In the SetLayoutHorizontal call it is valid to call LayoutUtility.GetMinWidth, LayoutUtility.GetPreferredWidth,
-    /// and LayoutUtility.GetFlexibleWidth on the RectTransform of itself or any of its children.
+    ///     and LayoutUtility.GetFlexibleWidth on the RectTransform of itself or any of its children.
+    ///
     /// In the SetLayoutVertical call it is valid to call LayoutUtility.GetMinHeight, LayoutUtility.GetPreferredHeight,
-    /// and LayoutUtility.GetFlexibleHeight on the RectTransform of itself or any of its children.
+    ///     and LayoutUtility.GetFlexibleHeight on the RectTransform of itself or any of its children.
     ///
     /// The component may use this information to determine the width and height to use for its own RectTransform
-    /// or the RectTransforms of its children.
+    ///     or the RectTransforms of its children.
     /// </remarks>
     public interface ILayoutController
     {
@@ -185,17 +186,18 @@ namespace UnityEngine.UI
     /// <remarks>
     /// The ILayoutSelfController derives from the base controller [[ILayoutController]] and controls the layout of a RectTransform.
     ///
-    /// Use the ILayoutSelfController to manipulate a GameObject’s own RectTransform component, which you attach in the Inspector.
+    /// Use ILayoutSelfController to manipulate a GameObject’s own RectTransform component, which you attach in the Inspector.
     /// Use ILayoutGroup to manipulate RectTransforms belonging to the children of the GameObject.
     ///
     /// Call ILayoutController.SetLayoutHorizontal to handle horizontal parts of the layout,
-    /// and call ILayoutController.SetLayoutVertical to handle vertical parts.
+    ///     and call ILayoutController.SetLayoutVertical to handle vertical parts.
+    ///
     /// You can change the height, width, position and rotation of the RectTransform.
     /// </remarks>
     /// <example>
     /// <code>
     /// //This script shows how the GameObject’s own RectTransforms can be changed.
-    /// //This creates a rectangle on the screen of the scale, positition and rotation you define in the Inspector.
+    /// //This creates a rectangle on the screen of the scale, position and rotation you define in the Inspector.
     /// //Make sure to set the X and Y scale to be more than 0 to see it
     ///
     /// using UnityEngine;
@@ -263,17 +265,18 @@ namespace UnityEngine.UI
     /// A RectTransform will be ignored by the layout system if it has a component which implements ILayoutIgnorer.
     /// </summary>
     /// <remarks>
-    /// A components that implements ILayoutIgnorer can be used to make a parent layout group component not consider
-    /// this RectTransform part of the group.
+    /// A component that implements ILayoutIgnorer can be used to make a parent ILayoutGroup component not consider
+    ///     this RectTransform as part of the group.
+    ///
     /// The RectTransform can then be manually positioned despite being a child GameObject of a layout group.
     /// </remarks>
     public interface ILayoutIgnorer
     {
         /// <summary>
-        /// Should this RectTransform be ignored bvy the layout system?
+        /// Should this RectTransform be ignored by the layout system?
         /// </summary>
         /// <remarks>
-        /// Setting this property to true will make a parent layout group component not consider this RectTransform part of the group.
+        /// Setting this property to true will make a parent ILayoutGroup component not consider this RectTransform part of the group.
         /// The RectTransform can then be manually positioned despite being a child GameObject of a layout group.
         /// </remarks>
         bool ignoreLayout { get; }
